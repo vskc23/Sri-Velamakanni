f7c2a9b7d3a287e6a890f69b1b442856c2cffd1b
vskc23 <kckrishna234@gmail.com>
2024-07-22 23:21:30 -0400

    Updates

---
+++ precache-manifest.182e65ee1097c582f440bd4b2e574e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb19c4e35cc08a01e023bf4d8a40815",
+    "url": "/Sri-Velamakanni/index.html"
+  },
+  {
+    "revision": "119972bc3243c2c0e126",
+    "url": "/Sri-Velamakanni/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "b34c48389fff9303bad8",
+    "url": "/Sri-Velamakanni/static/js/2.41d31066.chunk.js"
+  },
+  {
+    "revision": "d7b7536a0d6294158c77",
+    "url": "/Sri-Velamakanni/static/js/3.d6a3adab.chunk.js"
+  },
+  {
+    "revision": "119972bc3243c2c0e126",
+    "url": "/Sri-Velamakanni/static/js/main.0440269a.chunk.js"
+  },
+  {
+    "revision": "c5803628bdafa4705c37",
+    "url": "/Sri-Velamakanni/static/js/runtime~main.47981434.js"
+  }
+]);
